6957ad81c5827bf9caa933b992e3370dba80be6b
wittyambivert <wittyambivert@gmail.com>
2023-06-17 15:52:05 +0100

    boilerplate ready

---
+++ README.md
@@ -0,0 +1,1 @@
+# This file includes boilerplate files for vite-react-tailwind-react-icons
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/public";
+
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Home</div>
+    </>
+  );
+};
